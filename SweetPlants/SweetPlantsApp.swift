a31535f27203d6ce956a850c64fab725f16c839c
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-15 15:27:19 +0100

    Initial Commit

---
+++ SweetPlants/SweetPlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetPlantsApp.swift
+//  SweetPlants
+//
+//  Created by Andrej Kling on 15.12.20.
+//
+
+import SwiftUI
+
+@main
+struct SweetPlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
